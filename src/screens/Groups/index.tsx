581741010a27f4cf74897b4a3309bba5b84cb8f6
jpfeijo <j.feijo@edu.pucrs.br>
2023-01-17 11:02:03 -0300

    confirm and delete button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,10 +6,12 @@ import { Container  } from './styles';
 //import * as S from './styles';
 import { GroupCard } from '@components/GroupCard';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos']);
+  const [groups, setGroups] = useState<string[]>([]);
   
   return (
     <Container>
@@ -25,6 +27,16 @@ export function Groups() {
           <GroupCard title={item}
           />
         )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Que tal cadastrar a primeira turma?"
+          />
+        )}
+      />
+
+      <Button 
+        title="Criar nova turma"
       />
     </Container>
   );
